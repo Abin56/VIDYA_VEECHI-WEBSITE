4402e3fe3856d081e36943de19496337675ebd1b
SruthiSadanandan <sruthipariyapuram@gmail.com>
2024-03-21 15:46:46 +0530

    21/03/24 3:46

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vidyaveechi_website/firebase_options.dart';
-import 'package:vidyaveechi_website/view/home/home.dart';
 import 'package:vidyaveechi_website/view/home/screens/footer/privacypolicy.dart';
 import 'package:vidyaveechi_website/view/splash_screen/splash_screen.dart';
 import 'package:vidyaveechi_website/view/utils/shared_pref/user_auth/user_credentials.dart';
-// import 'package:vidyaveechi_website/view/home/admin/admin_home.dart';
+// import 'package:vidyaveechi_website/view/home/admin/admin_home.dart'
 import 'package:vidyaveechi_website/view/widgets/scroll_on_web_widget.dart';
 
 Future<void> main() async {
