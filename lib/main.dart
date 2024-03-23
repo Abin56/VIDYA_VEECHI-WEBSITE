c3b4d4ff675dd5644d1ba8918ad0a89d068c708d
Abin56 <abinjohn8089@gmail.com>
2024-03-23 10:56:26 +0530

    2 march second commit arun code pulled by edwin

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vidyaveechi_website/firebase_options.dart';
-import 'package:vidyaveechi_website/view/home/home.dart';
 import 'package:vidyaveechi_website/view/home/screens/footer/privacypolicy.dart';
 import 'package:vidyaveechi_website/view/splash_screen/splash_screen.dart';
 import 'package:vidyaveechi_website/view/utils/shared_pref/user_auth/user_credentials.dart';
-// import 'package:vidyaveechi_website/view/home/admin/admin_home.dart';
 import 'package:vidyaveechi_website/view/widgets/scroll_on_web_widget.dart';
 
 Future<void> main() async {
@@ -36,16 +34,7 @@ class MyApp extends StatelessWidget {
               '/spashscreen': (context) => SplashScreen(),
               '/PrivacyPolicy': (context) => const PrivacyPolicy(),
             },
-            //         getPages: [
-            //           GetPage(name: '/spashscreen', page: () => SplashScreen()),
-            //           // GetPage(name: '/Homescreen', page: () => const HomeScreen()),
-            //           GetPage(
 
-            // // GetPage(name: '/spashscreen', page: () => SplashScreen()),
-            //               participatesInRootNavigator: false,
-            //               name: '/PrivacyPolicy',
-            //               page: () => const PrivacyPolicy()),
-            //         ],
             home: SplashScreen(),
             theme: ThemeData(
                 inputDecorationTheme: InputDecorationTheme(
